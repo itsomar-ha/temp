1f40629f198376a3b9660a675bb63e5b95ddef17
itsomar-ha <100915574+itsomar-ha@users.noreply.github.com>
2025-04-13 17:17:22 +0200

    without points sections

--- app_rep.py
+++ app_rep.py
@@ -57,14 +57,10 @@ def calculate_player_scores(data):
     df['player_1_Score_Percentage'] = (df['player_1_Total_Score'] / max_score) * 100
     df['player_2_Score_Percentage'] = (df['player_2_Total_Score'] / max_score) * 100
 
-    player_1_data = df[['player_1_Score_Percentage', 'player_1_Total_Score'] + [col for col in df.columns if col.startswith('player_1_')]].to_dict(orient='records')[0]
-    player_2_data = df[['player_2_Score_Percentage', 'player_2_Total_Score'] + [col for col in df.columns if col.startswith('player_2_')]].to_dict(orient='records')[0]
+    player_1_data = df[['player_1_Score_Percentage', 'player_1_Total_Score'] + [col for col in df.columns if col.startswith('player_1_') and 'Points' not in col]].to_dict(orient='records')[0]
+    player_2_data = df[['player_2_Score_Percentage', 'player_2_Total_Score'] + [col for col in df.columns if col.startswith('player_2_') and 'Points' not in col]].to_dict(orient='records')[0]
 
-    #with open('player_1_data.json', 'w') as file_1:
-      #  print(json.dump(player_1_data, file_1, indent=4))
-  
-    #with open('player_2_data.json', 'w') as file_2:
-     #   print(json.dump(player_2_data, file_2, indent=4))
+
     print("Player 1 Data:")
     print(json.dumps(player_1_data, indent=4))
     print("\nPlayer 2 Data:")
